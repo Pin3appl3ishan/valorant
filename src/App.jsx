e723388088e86103aef43af80c45782912029c74
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-11-24 21:17:53 +0545

    about section barebone

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import Hero from './components/Hero'
+import About from "./components/About";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
-    <main className='relative min-h-screen w-screen overflow-x-hidden'>
-      <Hero/>
-
-      <section className='z-0 min-h-screen bg-blue-500'/>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
+      <About />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/About.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div id="about" className="min-h-screen w-screen">
+      <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
+        <h2 className="font-general text-sm uppercase md:text-[10px]">
+          Welcome to Valorant
+        </h2>
+
+        <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
+          Disc<b>o</b>ver the <br /> world's <br /> l<b>a</b>rgest shared
+          adventure
+        </div>
+
+        <div className="about-subtext">
+          <p>The Game of Games begins—your life, now an epic MMORPG</p>
+          <p>Valorant unites every player from countless games and platforms</p>
+        </div>
+      </div>
+
+      <div className="h-dvh w-screen" id="clip">
+        <div className="mask-clip-path about-image">
+          <img
+            src="img/about.webp"
+            alt="Background"
+            className="absolute left-0 size-full object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
