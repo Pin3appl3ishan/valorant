96e8d4d8232b43b42dc4960980cae38033490e58
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-11-24 21:32:08 +0545

    create reusable title component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,6 +2,7 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 
 import { ScrollTrigger } from "gsap/all";
+import AnimatedTitle from "./AnimatedTitle";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -32,10 +33,7 @@ const About = () => {
           Welcome to Valorant
         </h2>
 
-        <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
-          Disc<b>o</b>ver the <br /> world's <br /> l<b>a</b>rgest shared
-          adventure
-        </div>
+        <AnimatedTitle/>
 
         <div className="about-subtext">
           <p>The Game of Games begins—your life, now an epic MMORPG</p>
