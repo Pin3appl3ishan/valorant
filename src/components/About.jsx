0dbee3eec0f0b71009c79ec726499c61fea678bc
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-11-24 21:54:18 +0545

    add animation on title

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -33,7 +33,10 @@ const About = () => {
           Welcome to Valorant
         </h2>
 
-        <AnimatedTitle/>
+        <AnimatedTitle
+          title="Disc<b>o</b>ver the <br /> world's <br /> l<b>a</b>rgest shared adventure"
+          containerClass="mt-5 !text-black text-center"
+        />
 
         <div className="about-subtext">
           <p>The Game of Games begins—your life, now an epic MMORPG</p>
