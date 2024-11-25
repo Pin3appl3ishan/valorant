143627bef4c27d493ec922cc4282c48cd3c8fc2c
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-11-25 23:48:07 +0545

    navigation links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,8 @@ import { useRef } from "react";
 import Button from "./Button";
 import { TiLocationArrow } from "react-icons/ti";
 
+const navItems = ["Nexus", "Vault", "Prologue", "About", "Contact"];
+
 const Navbar = () => {
   const navContainerRef = useRef(null);
 
@@ -11,16 +13,32 @@ const Navbar = () => {
       className="fixed inset-x-0 top-4 z-50 h-16 border-none transition-all duration-700 sm:inset-x-6"
     >
       <header className="absolute top-1/2 w-full justify-between p-4">
-        <div className="flex items-center gap-7">
-          <img src="/img/logo.png" alt="logo" className="w-10" />
+        <nav className="flex size-full items-center justify-between p-4">
+          <div className="flex items-center gap-7">
+            <img src="/img/logo.png" alt="logo" className="w-10" />
 
-          <Button
-            id="product-button"
-            title="Products"
-            rightIcon={<TiLocationArrow />}
-            containerClass="bg-blue-50 md:flex hidden items-center justify-center gap-1"
-          />
-        </div>
+            <Button
+              id="product-button"
+              title="Products"
+              rightIcon={<TiLocationArrow />}
+              containerClass="bg-blue-50 md:flex hidden items-center justify-center gap-1"
+            />
+          </div>
+
+          <div className="flex h-full items-center">
+            <div className="hidden md:block">
+              {navItems.map((item) => (
+                <a
+                  key={item}
+                  href={`#${item.toLowerCase()}`}
+                  className="nav-hover-btn"
+                >
+                  {item}
+                </a>
+              ))}
+            </div>
+          </div>
+        </nav>
       </header>
     </div>
   );
