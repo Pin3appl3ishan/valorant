e55c431db7973e422a1ae4d0a955c40f9076e2b7
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-11-27 23:57:40 +0545

    add bento title and description

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,4 +1,4 @@
-const BentoCard = ({ src, title, description, isComingSoon }) => {
+const BentoCard = ({ src, title, description }) => {
   return (
     <div className="relative size-full">
       <video
@@ -10,7 +10,10 @@ const BentoCard = ({ src, title, description, isComingSoon }) => {
       />
       <div className="realtive z-10 flex size-full flex-col justify-between p-5 text-blue-50">
         <div>
-          <h1 className="bento-tite special-font">{title}</h1>
+          <h1 className="bento-title special-font">{title}</h1>
+          {description && (
+            <p className="mt-3 max-w-64 text-xs md:text-base">{description}</p>
+          )}
         </div>
       </div>
     </div>
@@ -25,12 +28,12 @@ const Features = () => {
           <p className="font-circular-web text-lg text-blue-50">
             Into the Metagame Layer
           </p>
+          <p className="max-w-md font-circular-web text-lg text-blue-50 opacity-50">
+            Immerse yourself in a rich and ever-expanding universe where a
+            vibrant array of products converge into an interconnected overlay
+            experience on your world.
+          </p>
         </div>
-        <p className="max-w-md font-circular-web text-lg text-blue-50 opacity-50">
-          Immerse yourself in a rich and ever-expanding universe where a vibrant
-          array of products converge into an interconnected overlay experience
-          on your world.
-        </p>
       </div>
 
       <div className="border-hsla mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
@@ -38,11 +41,10 @@ const Features = () => {
           src="videos/feature-1.mp4"
           title={
             <>
-              radi<b>n</b>t
+              radia<b>n</b>t
             </>
           }
           description="A cross-platform metagame app, turning your activities across Web2 and Web3 games into a rewarding adventure."
-          isComingSoon={true}
         />
       </div>
     </section>
