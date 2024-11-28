d40d6b848724099dbdabcf0b8d230e47e26c575b
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-11-28 22:47:46 +0545

    add toast with button

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -2,6 +2,7 @@ import gsap from "gsap";
 import { useRef } from "react";
 import AnimatedTitle from "./AnimatedTitle";
 import RoundedCorners from "./RoundedCorners";
+import Button from "./Button";
 
 const Story = () => {
   const frameRef = useRef("null");
@@ -73,8 +74,23 @@ const Story = () => {
               </div>
             </div>
 
-            <RoundedCorners/>
-            
+            <RoundedCorners />
+          </div>
+        </div>
+
+        <div className="mt-80 flex w-full justify-center md:-mt-64 md:me-44 md:justify-end">
+          <div className="flex h-full w-fit flex-col items-center md:items-start">
+            <p className="mt-3 max-w-sm text-center font-circular-web text-violet-50 md:text-start">
+              Where realms converge, lies Valorant and the boundless pillar.
+              Discover its secrets and shape your fate amidst infinite
+              opportunities.
+            </p>
+
+            <Button
+              id="realm-btn"
+              title="discover prologue"
+              containerClass="mt-5"
+            />
           </div>
         </div>
       </div>
