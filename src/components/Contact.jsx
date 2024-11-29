6e9c560882678c106c93aee60907e8a7b424dc0f
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-11-29 22:44:12 +0545

    create ImageClipBox compoent

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,18 @@
 const Contact = () => {
-  
+  const ImageClipBox = ({ src, clipClass }) => (
+    <div className={clipClass}>
+      <img src={src} />
+    </div>
+  );
+
   return (
     <div id="contact" className="my-20 min-h-96 w-screen px-10">
       <div className="relative rounded-lg bg-black py-24">
         <div className="absolute -left-20 top-0 hidden h-full w-72 overflow-hidden sm:block lg:left-20 lg:w-96">
-
+          <ImageClipBox
+            src="img/contact-1.webp"
+            clipClass="contact-clip-path-1"
+          />
         </div>
       </div>
     </div>
