aaab42015d8279f56775366e0313234e765bcb2d
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-11-29 23:00:46 +0545

    add footer text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,15 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="w-screen bg-violet-300 py-4 text-black">
+      <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
+        <p className="text-center text-sm font-light md:text-left">
+          &copy; Nova 2024. All rights reserved
+        </p>
+
+
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
